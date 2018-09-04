4798b8a4c1373ebb2fddc5311ada7ce38bc696ea
Edgar Diaz <edgar.diaz@magmalabs.io>
2018-09-03 23:15:01 -0500

    Add solves for async testing

--- app/async.js
+++ app/async.js
@@ -2,10 +2,14 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
-
+    return new Promise((resolve, reject) => setTimeout(resolve(value), 100));
   },
 
   manipulateRemoteData: function(url) {
-
+    return new Promise((resolve, reject) => {
+      $.ajax(url).then((resp) => {
+        resolve(resp.people.map(person => person.name).sort());
+      });
+    });
   }
 };
