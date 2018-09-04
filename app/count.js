2933b4829bf72ee53fa38ff96efd8a9feb689027
Edgar Diaz <edgar.diaz@magmalabs.io>
2018-09-03 23:16:12 -0500

    Add solves for countant

--- app/count.js
+++ app/count.js
@@ -2,6 +2,17 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    var timeout;
+    var assign_num = () => {
+      console.log(start++);
+      if (start <= end)
+        timeout = setTimeout(assign_num, 100);
+    }
 
+    assign_num();
+
+    return {
+      cancel: () => timeout && clearTimeout(timeout)
+    }
   }
 };
